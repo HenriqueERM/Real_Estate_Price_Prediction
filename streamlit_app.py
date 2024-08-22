aaa9365cf8a11e859427949e16fd21006cf3d006
HenriqueERM <158082766+HenriqueERM@users.noreply.github.com>
2024-08-22 01:18:44 +0000

    git add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,23 @@
+
+# Load and Explore Data:
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import requests as req
+
+import matplotlib.pyplot as plt
+import scipy.special as special
+
+from scipy import stats
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 
 st.title("🎈 My new app")
 st.write(
